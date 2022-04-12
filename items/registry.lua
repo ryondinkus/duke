9c1206e4c49e79b5057eafd258eb3ea1f50b6961
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2022-04-12 00:38:24 -0400

    add lil duke

--- items/registry.lua
+++ items/registry.lua
@@ -1,5 +1,6 @@
 DukeHelpers.Items = {
     dukesGullet = include("items/dukesGullet"),
     othersGullet = include("items/othersGullet"),
-    thePrinces = include("items/thePrinces")
+    thePrinces = include("items/thePrinces"),
+	lilDuke = include("items/lilDuke")
 }
