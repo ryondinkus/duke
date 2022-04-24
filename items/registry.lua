eb880019d365b4c342b49c07bea341ea3412f6b6
Noah Richardson <ngregrichardson@gmail.com>
2022-04-24 18:46:24 -0400

    Implement super infestation

--- items/registry.lua
+++ items/registry.lua
@@ -2,6 +2,7 @@ DukeHelpers.Items = {
     dukesGullet = include("items/dukesGullet"),
     othersGullet = include("items/othersGullet"),
     thePrinces = include("items/thePrinces"),
-	lilDuke = include("items/lilDuke"),
-	dukeFlute = include("items/dukeFlute")
+    lilDuke = include("items/lilDuke"),
+    dukeFlute = include("items/dukeFlute"),
+    superInfestation = include("items/superInfestation")
 }
