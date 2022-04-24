5b528e9035d5285219f835183a6454c95407f96c
Noah Richardson <ngregrichardson@gmail.com>
2022-04-24 17:55:13 -0400

    Only allow infested heart to run when hearts are pick-up-able

--- trinkets/infestedHeart.lua
+++ trinkets/infestedHeart.lua
@@ -9,11 +9,27 @@ local Descriptions = {
     en_us = "It's buzzing...",
     spa = "Esta zumbando..."
 }
-local WikiDescription = "It's buzzing..."--helper.GenerateEncyclopediaPage("Poops and shits everywhere.")
+local WikiDescription = "It's buzzing..." --helper.GenerateEncyclopediaPage("Poops and shits everywhere.")
 
 local function RandomlySpawnHeartFlyFromPickup(player, pickup)
     if player and player:HasTrinket(Id) then
-        if DukeHelpers.PercentageChance(50, 100) then
+        local canPickup = true
+
+        if pickup.SubType == HeartSubType.HEART_FULL or pickup.SubType == HeartSubType.HEART_HALF then
+            canPickup = player:CanPickRedHearts()
+        elseif pickup.SubType == HeartSubType.HEART_SOUL or pickup.SubType == HeartSubType.HEART_HALF_SOUL then
+            canPickup = player:CanPickSoulHearts()
+        elseif pickup.SubType == HeartSubType.HEART_BLACK then
+            canPickup = player:CanPickBlackHearts()
+        elseif pickup.SubType == HeartSubType.HEART_BONE then
+            canPickup = player:CanPickBoneHearts()
+        elseif pickup.SubType == HeartSubType.HEART_ROTTEN then
+            canPickup = player:CanPickRottenHearts()
+        elseif pickup.SubType == HeartSubType.HEART_GOLDEN then
+            canPickup = player:CanPickGoldenHearts()
+        end
+
+        if canPickup and DukeHelpers.PercentageChance(50, 100) then
             local heartSubType = DukeHelpers.GetFlyByPickupSubType(pickup.SubType)
             if pickup.SubType == HeartSubType.HEART_BLENDED then
                 if DukeHelpers.PercentageChance(50, 100) then
@@ -56,7 +72,7 @@ return {
     Name = Name,
     Names = Names,
     Tag = Tag,
-	Id = Id,
+    Id = Id,
     Descriptions = Descriptions,
     WikiDescription = WikiDescription,
     callbacks = {
