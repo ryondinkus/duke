0f7ac89e9a337cd3b373b3e12d3c7a90843e42e5
Noah Richardson <ngregrichardson@gmail.com>
2022-06-10 09:30:36 -0400

    Add startup filled slots

--- husk.lua
+++ husk.lua
@@ -130,5 +130,6 @@ function DukeHelpers.InitializeHusk(p, continued)
 end
 
 function DukeHelpers.AddStartupSpiders(player)
+	DukeHelpers.FillRottenGulletSlot(player, DukeHelpers.Spiders.RED.pickupSubType, 1)
 	DukeHelpers.SpawnSpidersFromPickupSubType(HeartSubType.HEART_FULL, player.Position, player, 2)
 end
