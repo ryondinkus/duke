e1581790f461b5d36862adccb9913925a3163b3c
basketini-jones <60999059+basketini-jones@users.noreply.github.com>
2022-06-15 17:16:30 -0400

    sewing machine support

--- entityVariants/lilHusk.lua
+++ entityVariants/lilHusk.lua
@@ -11,6 +11,8 @@ local DIRECTION = {
 
 local fireCooldown = 19
 local lullabyFireCooldown = 7
+local spiderLimit = 1
+local superSpiderLimit = 2
 
 local function MC_FAMILIAR_INIT(_, familiar)
 	familiar:AddToFollowers()
@@ -22,15 +24,17 @@ local function MC_FAMILIAR_UPDATE(_, familiar)
 	local player = familiar.Player
 	local fireDirection = player:GetFireDirection()
 	local fireDirectionSprite = DIRECTION[fireDirection]
+
 	if familiar.FrameCount == 6 then
 		data.fireCooldown = 0
-		data.canSpawnSpider = true
+		data.canSpawnSpider = spiderLimit
 	end
+
 	if fireDirection == Direction.NO_DIRECTION then
 		sprite:Play("FloatDown", false)
 	else
 		if data.fireCooldown <= 0 then
-			if data.canSpawnSpider then
+			if data.canSpawnSpider > 0 then
 				sprite:Play("Shoot" .. fireDirectionSprite, false)
 			end
 		else
@@ -39,35 +43,49 @@ local function MC_FAMILIAR_UPDATE(_, familiar)
 			sprite:SetFrame(frame)
 		end
 	end
+
 	if sprite:IsEventTriggered("Barf") then
 		DukeHelpers.sfx:Play(SoundEffect.SOUND_WORM_SPIT, 1, 0, false, 1.5)
 		local effect = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, 0, familiar.Position, Vector.Zero, nil)
 		effect.Color = Color(0, 0, 0, 1)
 		effect.SpriteScale = Vector(0.5, 0.5)
-		local spawnedSpider = DukeHelpers.SpawnSpidersFromPickupSubType(DukeHelpers.GetWeightedSpider(DukeHelpers.rng).pickupSubType,
-			familiar.Position, familiar, 1, true)
+
+		local spiderType = DukeHelpers.GetWeightedSpider(DukeHelpers.rng).pickupSubType
+		local spawnedSpider = DukeHelpers.SpawnSpidersFromPickupSubType(spiderType, familiar.Position, familiar, 1, true)
+		local spawnedFly = nil
+
+		if Sewn_API and Sewn_API:IsUltra(data) then
+			spawnedFly = DukeHelpers.SpawnAttackFlyBySubType(spiderType, familiar.Position, familiar.Player)
+		end
+
 		if familiar.Player and familiar.Player:HasCollectible(CollectibleType.COLLECTIBLE_BFFS) and not familiar.Player:HasCollectible(CollectibleType.COLLECTIBLE_HIVE_MIND) then
 			spawnedSpider[1]:GetData().bffs = true
 			spawnedSpider[1].CollisionDamage = spawnedSpider[1].CollisionDamage * 2
+			if spawnedFly then
+				spawnedFly:GetData().bffs = true
+				spawnedFly.CollisionDamage = spawnedFly.CollisionDamage * 2
+			end
 		end
+
 		if player:HasTrinket(TrinketType.TRINKET_FORGOTTEN_LULLABY) then
 			data.fireCooldown = lullabyFireCooldown
 		else
 			data.fireCooldown = fireCooldown
 		end
-		data.canSpawnSpider = false
+		
+		data.canSpawnSpider = data.canSpawnSpider - 1
 	end
 
-	if data.canSpawnSpider then
+	if data.canSpawnSpider and data.canSpawnSpider > 0 then
 		data.fireCooldown = data.fireCooldown - 1
 	end
+
 	familiar:FollowParent()
-	print(data.fireCooldown)
 end
 
 local function MC_POST_ENTITY_REMOVE(_, e)
     if e.Variant == FamiliarVariant.BLUE_SPIDER and e.SpawnerEntity and e.SpawnerType == EntityType.ENTITY_FAMILIAR and e.SpawnerVariant == Id then
-        e.SpawnerEntity:GetData().canSpawnSpider = true
+        e.SpawnerEntity:GetData().canSpawnSpider = e.SpawnerEntity:GetData().canSpawnSpider + 1
     end
 end
 
@@ -79,6 +97,11 @@ end
 
 if Sewn_API then
 	Sewn_API:MakeFamiliarAvailable(Id, DukeHelpers.Items.lilHusk.Id)
+	Sewn_API:AddCallback(Sewn_API.Enums.ModCallbacks.ON_FAMILIAR_UPGRADED, function(_, familiar)
+		if familiar:GetData() then
+			familiar:GetData().canSpawnSpider = superSpiderLimit
+		end
+	end, Id)
 end
 
 return {
